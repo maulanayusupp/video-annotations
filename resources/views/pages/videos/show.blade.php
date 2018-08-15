b44caa0e71ba8baaca6adc801e36007ecccdaa14
Maulana Yusup Abdullah <maulanayusupp@gmail.com>
2018-08-15 08:44:39 +0700

    Update

--- resources/views/pages/videos/show.blade.php
+++ resources/views/pages/videos/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">
                     <a href="{{ url('/admin/videos') }}" class="btn btn-primary btn-sm float-left">Back</a>
